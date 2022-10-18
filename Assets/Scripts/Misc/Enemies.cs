b92ce9cfde6456102b26b48040fa13dd41ce6574
MrMFWIC <Benstar02@hotmail.ca>
2022-10-17 21:50:48 -0300

    Added Player w. Camera and Movement, Added Enemies and Enemy Spawning

---
+++ Assets/Scripts/Misc/Enemies.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public enum EnemyTypes
+    {
+        Chest,
+        Floater,
+        Slime,
+        Spike,
+        GrWizard,
+        BlWizard
+    }
+
+    public EnemyTypes currentEnemyType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
